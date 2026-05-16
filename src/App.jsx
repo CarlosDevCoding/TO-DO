02155e2cb4975cc2d2091a26bc2479accfc8dea6
Carlos Ibanez <carlos.ibanez@live.se>
2026-05-16 23:34:13 +0200

    fixat buggar och annat

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,44 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
   const [text, setText] = useState('')
+  const[task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      setTasks([...tasks, task]);
+      setTask("");
+    }
+  };
+
   return (
-    <>
-    <br></br><div>Välkommen till min TO-DO app. <br></br>Byggd 100% för hand av en människa.<br></br>
-          Vad vill du anteckna idag?          <h2>Anteckningar</h2>
-          <div>
-            <textarea value={text} onChange={(e) => setText(e.target.value)} />
+    <> 
+    <input
+      type="text"
+      placeholder="Skriv en uppgift...."
+      value={task}
+      onChange={(e) => setTask(e.target.value)}
+      onKeyDown={handleKeyDown}
+      />
+         <div>
+          <h1>Välkommen till min TO-DO app.</h1>
+            
+             <p>Byggd 100% för hand av en människa. 
+              <br />
+          Vad vill du anteckna idag?
+          </p>    
+                    <h2>Anteckningar</h2>
+          
+            <textarea
+             value={text}
+             onChange={(e) => setText(e.target.value)} 
+            />
           </div>
-    </div>
-            </>
+    
+    </>
   )
 }
 
