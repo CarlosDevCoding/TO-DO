a59492519bc6eb61b37b70ee0292565b760f672f
Carlos Ibanez <carlos.ibanez@live.se>
2026-05-17 16:08:27 +0200

    Connectad till MongoDB

--- server/index.js
+++ server/index.js
@@ -1,4 +1,7 @@
 require("dotenv").config();
+
+console.log("Server is starting..."); 
+console.log(process.env.MONGODB_URI);
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
@@ -8,10 +11,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
+mongoose.connect(process.env.MONGODB_URI);
 
 app.get("/", (req, res) => {
   res.send("API is running");
